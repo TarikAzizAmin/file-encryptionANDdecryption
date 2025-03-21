ae8e71339f06c33c697f6158c527d421b68f9dfe
TarikAzizAmin <88424929+TarikAzizAmin@users.noreply.github.com>
2025-03-21 21:10:08 +0530

    Update Decrypt.txt.py

--- Decrypt.txt.py
+++ Decrypt.txt.py
@@ -12,13 +12,15 @@ fernet = Fernet(get_key())
 
 
 def decrypt_file(file):
-    key = get_key()
-    with open(file, "rb") as file:
-        content = file.read()
-    decrypted_content = fernet.decrypt(content)
-    with open(file, "wb") as file:
-        file.write(decrypted_content)
-
+    try:
+        key = get_key()
+        with open(file, "rb") as f1:
+            content = f1.read()
+        decrypted_content = fernet.decrypt(content)
+        with open(file, "wb") as f2:
+            f2.write(decrypted_content)
+    except Exception as e:
+        print(f" An error occured while decrypting file: {str(e))
 if __name__ == "__main__":
     filePath = input("Please enter the file location for decrypt\n")
     decrypt_file(filePath)
